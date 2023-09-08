4195c96728bb65e88335e8c151c8f5fb2fc885d4
dx_sv_note <drakonfreshr@gmail.com>
2023-09-08 22:22:46 +0300

    Add statistics, api and components

---
+++ ShopAPI/ShopAPI/Model/Entities/DTO/Statistics/BaseStatisticsDTO.cs
@@ -0,0 +1,12 @@
+﻿using ShopApi.Model.Entities.DTO.ProductSearch;
+
+namespace ShopApi.Model.Entities.DTO.Statistics
+{
+    public class BaseStatisticsDTO
+    {
+        public int OrderCount {  get; set; }
+        public float AverageCheck { get; set; }
+        public float SumOfOrders { get; set; }
+        public ICollection<SimpleProductDTO> MostPopularProducts { get; set; }
+    }
+}
---
+++ ShopAPI/ShopAPI/Model/Interfaces/Repository/IStatisticsRepository.cs
@@ -0,0 +1,9 @@
+﻿using ShopApi.Model.Entities.DTO.Statistics;
+
+namespace ShopApi.Model.Interfaces.Repository
+{
+    public interface IStatisticsRepository
+    {
+        Task<BaseStatisticsDTO> GetBaseStatistics(DateTime beginingPeriod, DateTime endPeriod);
+    }
+}
