4470fcf2b6b65ed97b91c271f69ec848fe672427
dx_sv_note <drakonfreshr@gmail.com>
2023-09-08 22:24:02 +0300

    Revert "Add order field to OrderItem"
    This reverts commit 94db5cb56d9bb79d19591c6ae309fda1c5203940.

--- ShopAPI/ShopAPI/Model/Interfaces/Repository/IStatisticsRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using ShopApi.Model.Entities.DTO.Statistics;
-
-namespace ShopApi.Model.Interfaces.Repository
-{
-    public interface IStatisticsRepository
-    {
-        Task<BaseStatisticsDTO> GetBaseStatistics(DateTime beginingPeriod, DateTime endPeriod);
-    }
-}
