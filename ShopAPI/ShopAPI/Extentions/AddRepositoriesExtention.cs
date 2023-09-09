be4af3bc108c3e43125dce1c6cd4e2207c980593
dx_sv_note <drakonfreshr@gmail.com>
2023-09-09 21:15:55 +0300

    add repository ti injector

--- ShopAPI/ShopAPI/Extentions/AddRepositoriesExtention.cs
+++ ShopAPI/ShopAPI/Extentions/AddRepositoriesExtention.cs
@@ -19,6 +19,8 @@ namespace ShopApi.Extentions
             services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>();
 
             services.AddScoped<IOrderRepository, OrderRepository>();
+
+            services.AddScoped<IStatisticsRepository, StatisticsRepository>();
         }
     }
 }
