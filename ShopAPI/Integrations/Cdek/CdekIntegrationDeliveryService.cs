d255389eec3f80b2e3b8d9192f0f6be118e4910a
drr <drr@DESKTOP-4VLSIMM>
2023-10-01 21:12:21 +0300

    update cdek root

---
+++ ShopAPI/Integrations/Cdek/CdekIntegrationDeliveryService.cs
@@ -0,0 +1,13 @@
+﻿using ShopApi.Model.Interfaces.Services;
+using ShopDb.Entities;
+
+namespace Integrations.Cdek
+{
+    public class CdekIntegrationDeliveryService : IDeliveryService
+    {
+        public Task TransferToDelivery(Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
