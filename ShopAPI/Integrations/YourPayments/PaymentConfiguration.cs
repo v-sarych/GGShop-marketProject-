dd8dd6f8950fae2ee8f7c206533ad1a4ff085426
dx_sv_note <drakonfreshr@gmail.com>
2023-10-02 11:37:43 +0300

    create your payment integration

---
+++ ShopAPI/Integrations/YourPayments/PaymentConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integrations.YourPayments
+{
+    internal class PaymentConfiguration
+    {
+        public readonly string MerchantId = "merchant";
+        public readonly string Signature = "key";
+
+        public PaymentConfiguration() { }
+    }
+}
---
+++ ShopAPI/Integrations/YourPayments/YourPaymentIntegrationPaymentService.cs
@@ -0,0 +1,13 @@
+﻿using ShopApi.Model.Entities.Payments;
+using ShopApi.Model.Interfaces.Services;
+
+namespace Integrations.YourPayments
+{
+    internal class YourPaymentIntegrationPaymentService : IPaymentService
+    {
+        public Task<string> CreateAndAuthorizePayment(PaymentInfoDTO info)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
