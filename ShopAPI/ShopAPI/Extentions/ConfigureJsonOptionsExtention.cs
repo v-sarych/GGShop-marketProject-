df2aa1a6c8cd830d4cf1b1381fb288f6ff76f9b0
dx_sv_note <drakonfreshr@gmail.com>
2023-10-22 23:01:51 +0300

    Add json configuration

---
+++ ShopAPI/ShopAPI/Extentions/ConfigureJsonOptionsExtention.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+
+namespace ShopApiServer.Extentions
+{
+    public static class ConfigureJsonOptionsExtention
+    {
+        public static void ConfigureJsonOptions(this WebApplicationBuilder builder)
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+        }
+    }
+}
