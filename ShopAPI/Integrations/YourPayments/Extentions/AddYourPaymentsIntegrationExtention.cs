3482b2daa9bd898e1d9e9bf0494d94ba03125a0e
dx_sv_note <drakonfreshr@gmail.com>
2023-11-10 12:14:21 +0300

    add your payments extention

---
+++ ShopAPI/Integrations/YourPayments/Extentions/AddYourPaymentsIntegrationExtention.cs
@@ -0,0 +1,24 @@
+﻿using Integrations.YourPayments.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using ShopApiCore.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integrations.YourPayments.Extentions
+{
+    public static class AddYourPaymentsIntegrationExtention
+    {
+        public static IServiceCollection AddYourPaymentsIntegration(this IServiceCollection services)
+        {
+            services.AddSingleton<PaymentConfiguration>(new PaymentConfiguration());
+
+            services.AddScoped<IPaymentDataCreator, PaymentDataCreator>();
+            services.AddScoped<IPaymentService, YourPaymentIntegrationPaymentService>();
+
+            return services;
+        }
+    }
+}
