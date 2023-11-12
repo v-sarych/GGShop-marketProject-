b3dd5d7752f10203259096807a5b6a9fbf9d11ec
drr <drr@DESKTOP-4VLSIMM>
2023-11-12 13:19:31 +0300

    add reauired to dto

--- ShopAPI/ShopAPICore/Entities/DTO/ProductAvailability/CreateProductAvailabilityDTO.cs
+++ ShopAPI/ShopAPICore/Entities/DTO/ProductAvailability/CreateProductAvailabilityDTO.cs
@@ -1,14 +1,22 @@
-﻿namespace ShopApiCore.Entities.DTO.ProductAvailability
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShopApiCore.Entities.DTO.ProductAvailability
 {
     public class CreateProductAvailabilityDTO
     {
+        [Required]
         public string Sku {  get; set; }
+        [Required]
         public int Count { get; set; }
         public string Size { get; set; } = "without size";
+        [Required]
         public float Cost { get; set; }
+        [Required]
         public int ProductId { get; set; }
 
+        [Required]
         public int Weight {  get; set; }
+        [Required]
         public int PackageSizeId {  get; set; }
     }
 }
