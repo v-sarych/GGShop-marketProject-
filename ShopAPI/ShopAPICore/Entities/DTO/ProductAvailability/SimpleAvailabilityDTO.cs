d532c2009df19aeadeb205c54fb39b60951ea95a
dx_sv_note <drakonfreshr@gmail.com>
2023-12-15 09:17:31 +0300

    add sku to search result

--- ShopAPI/ShopAPICore/Entities/DTO/ProductAvailability/SimpleAvailabilityDTO.cs
+++ ShopAPI/ShopAPICore/Entities/DTO/ProductAvailability/SimpleAvailabilityDTO.cs
@@ -2,6 +2,7 @@
 {
     public class SimpleAvailabilityDTO
     {
+        public string Sku { get; set; }
         public string Size { get; set; }
         public float Cost { get; set; }
         public int Count { get; set; }
