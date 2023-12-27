b9b761842898a94a254ad07b0cd1c05d5239875b
dx_sv_note <drakonfreshr@gmail.com>
2023-12-27 10:51:35 +0300

    create deliveryInfo migration

---
+++ ShopAPI/ShopDb/Migrations/20231227075011_AddDeliveryInfoToOrder.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDeliveryInfoToOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "AdditionalOrderInfo",
+                table: "Orders",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeliveryInfo",
+                table: "Orders",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeliveryInfo",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AdditionalOrderInfo",
+                table: "Orders",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
